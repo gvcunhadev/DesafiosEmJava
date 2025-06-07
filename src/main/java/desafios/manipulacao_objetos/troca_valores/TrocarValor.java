03e848defae4851012ce0b619c5f7f580b571c27
Gabriela Viana Cunha <2411550012@fema.edu.br>
2025-06-07 14:00:17 -0300

    feat: adicionar atividade 3.2 troca de valores de objetos

---
+++ src/main/java/desafios/manipulacao_objetos/troca_valores/TrocarValor.java
@@ -0,0 +1,24 @@
+package desafios.manipulacao_objetos.troca_valores;
+
+public class TrocarValor {
+    private int valor;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public static void trocarValores(TrocarValor refA, TrocarValor refB){
+        int temp = refA.getValor();
+        refA.setValor(refB.getValor());
+        refB.setValor(temp);
+    }
+
+    public static void imprimirValores(TrocarValor a, TrocarValor b) {
+        System.out.println("objA.valor = " + a.getValor());
+        System.out.println("objB.valor = " + b.getValor());
+    }
+}
--- src/main/java/desafios/metodos/calculadora/Calculadora.java
+++ src/main/java/desafios/metodos/calculadora/Calculadora.java
@@ -4,11 +4,9 @@ public class Calculadora {
     public double somar(double x, double y) {
         return x + y;
     }
-
     public double subtrair(double x, double y) {
         return x - y;
     }
-
     public double multiplicar(double x, double y){
         return x * y;
     }
