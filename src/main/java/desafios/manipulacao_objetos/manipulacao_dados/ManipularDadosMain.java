079cec73bad8905b9cfde996acaa8b0ad7957f7b
Gabriela Viana Cunha <2411550012@fema.edu.br>
2025-06-07 11:04:03 -0300

    feat: adicionar atividade 3.1 manipulação de dados

---
+++ src/main/java/desafios/manipulacao_objetos/manipulacao_dados/ManipularDadosMain.java
@@ -0,0 +1,9 @@
+package desafios.manipulacao_objetos.manipulacao_dados;
+
+public class ManipularDadosMain {
+    public static void main(String[] args) {
+        Numero numero = new Numero();
+        numero.setValor(100);
+        numero.imprimirValor();
+    }
+}
---
+++ src/main/java/desafios/manipulacao_objetos/manipulacao_dados/Numero.java
@@ -0,0 +1,15 @@
+package desafios.manipulacao_objetos.manipulacao_dados;
+
+public class Numero {
+    private int valor;
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public void imprimirValor() {
+        System.out.println("Valor: " + valor);
+        System.out.println("Endereço de memória (simulado): " + System.identityHashCode(this.valor));
+    }
+}
+
