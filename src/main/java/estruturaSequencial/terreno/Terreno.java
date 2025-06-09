2e0e87837536eb449a0fd9cb5d6783660d6645ed
Gabriela Viana Cunha <2411550012@fema.edu.br>
2025-06-09 08:20:29 -0300

    feat: adicionar atividade 2.1 problema terreno

---
+++ src/main/java/estruturaSequencial/terreno/Terreno.java
@@ -0,0 +1,22 @@
+package estruturaSequencial.terreno;
+
+public class Terreno {
+
+    private float largura;
+    private float comprimento;
+    private float valorMetroQuadrado;
+
+    public Terreno(float largura, float comprimento, float valorMetroQuadrado) {
+        this.largura = largura;
+        this.comprimento = comprimento;
+        this.valorMetroQuadrado = valorMetroQuadrado;
+    }
+
+    public double calcularArea() {
+        return largura * comprimento;
+    }
+
+    public double calcularValorTotal() {
+        return calcularArea() * valorMetroQuadrado;
+    }
+}
