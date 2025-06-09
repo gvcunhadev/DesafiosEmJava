07b57c6234ecfdef0df4b576f7d5af49f1cd4ddf
Gabriela Viana Cunha <2411550012@fema.edu.br>
2025-06-09 08:21:18 -0300

    feat: adicionar atividade 2.3 problema idades

---
+++ src/main/java/estruturaSequencial/idades/Pessoa.java
@@ -0,0 +1,20 @@
+package estruturaSequencial.idades;
+
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int calcularMediaIdades(Pessoa outraPessoa) {
+        return (this.idade + outraPessoa.idade)/2;
+    }
+}
