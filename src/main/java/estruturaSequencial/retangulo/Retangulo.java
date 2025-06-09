39fae971924bad65be2d6f58eb8bcbba292ca70b
Gabriela Viana Cunha <2411550012@fema.edu.br>
2025-06-09 08:20:55 -0300

    feat: adicionar atividade 2.2 problema retangulo

---
+++ src/main/java/estruturaSequencial/retangulo/Retangulo.java
@@ -0,0 +1,24 @@
+package estruturaSequencial.retangulo;
+
+public class Retangulo {
+
+    private float altura;
+    private float base;
+
+    public Retangulo(float altura, float base) {
+        this.altura = altura;
+        this.base = base;
+    }
+
+    public double calcularArea() {
+        return altura * base;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * (altura + base);
+    }
+
+    public double calcularDiagonal() {
+        return Math.sqrt(Math.pow(altura, 2) + Math.pow(base, 2));
+    }
+}
